741a0e15c7e7dd8058543172d4a8c0cd2d872b80
z5231006 <z5231006@cse.unsw.edu.au>
2018-07-25 19:57:05 +1000

    branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,8 @@
-// A simple Java Program
+/**
+* A simple java program that prints a hello world message to the console
+*
+*/
+
 class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello, Welcome to COMP2511!");
