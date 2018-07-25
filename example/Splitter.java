539b8f211e91312fe07e39e5a93572bc97f4ac9e
z5231006 <z5231006@cse.unsw.edu.au>
2018-07-25 21:21:49 +1000

    bonus done

--- example/Splitter.java
+++ example/Splitter.java
@@ -1,9 +1,18 @@
 package example;
 
+import java.util.Scanner;
+
 public class Splitter {
 
 	public static void main(String[] args){
         System.out.println("Enter a sentence specified by spaces only: ");
         // Add your code
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();
+        String[] ans= s.split(" ");
+        for(int i=0;i<ans.length;i++) {
+        	System.out.println(ans[i]);
+        }
+        sc.close();
     }
 }
